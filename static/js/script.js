017c9d6969b59d151c99183070ed1dbad18eb0a1
Dzakus <Dzakus13@gmail.com>
2016-02-10 23:33:41 +0100

    Disable navbar script for page without introduction section

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,9 @@
 (function($) {
 	var navbar = $('.navbar');
 	var sectionIntroduction = $('.section--introduction');
-
+	if(sectionIntroduction == 0){
+		return;
+	}
 	function checkScroll(){
 		if(($(window).scrollTop() - sectionIntroduction.outerHeight() + navbar.outerHeight()) > 0) {
 			navbar.removeClass('navbar-opaque');
